9d2c8f3adc81f14173f96b7ff3e893b54cb7f940
Momchil Tomov <momchil@Momchils-MacBook-Air.local>
2017-09-07 20:06:57 -0400

    red green blue

--- plot_map.m
+++ plot_map.m
@@ -1,16 +1,41 @@
-function plot_map(map)
+function plot_map(map, color)
     reward_symbol = '$';
     empty_symbol = '.';
     wall_symbol = '#';
     agent_symbol = 'X';
-    imagesc(map == wall_symbol);
+
+    I = double(map ~= wall_symbol);
+    I = repmat(I, [1 1 3]);
+    grey = 0.3;
+    if isequal(color, 'red')
+        I(:,:,1) = I(:,:,1) * (1 - grey) + grey; % R
+        I(:,:,2) = grey; % G
+        I(:,:,3) = grey; % B
+    elseif isequal(color, 'green')
+        I(:,:,1) = grey; % R
+        I(:,:,2) = I(:,:,2) * (1 - grey) + grey; % G
+        I(:,:,3) = grey; % B
+    elseif isequal(color, 'blue')
+        I(:,:,1) = grey; % R
+        I(:,:,2) = grey; % G
+        I(:,:,3) = I(:,:,3) * (1 - grey) + grey; % B
+    else
+        I(:,:,1) = I(:,:,1) * (1 - grey) + grey; % B; % R
+        I(:,:,2) = I(:,:,1); % G
+        I(:,:,3) = I(:,:,1); % B
+    end
+
+    image(I);
+    axis off;
+
+
     font_size = 40;
     gap_size = 0.25;
     line_width = 5;
 
     % agent
     [ax, ay] = ind2sub(size(map), find(map == agent_symbol));
-    text(ay - gap_size, ax, 'X', 'FontSize', font_size, 'FontWeight', 'bold', 'Color', 'red');
+    text(ay - gap_size, ax, 'X', 'FontSize', font_size, 'FontWeight', 'bold', 'Color', 'black');
 
     % rewards 
     %[rx, ry] = ind2sub(size(map), find(map == reward_symbol));

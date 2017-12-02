1d7b19a3fc7885df2a731faa5f5c3cc40ea521a6
Momchil Tomov <tomov90@gmail.com>
2017-12-02 12:42:32 -0500

    DP with CRP

--- iPOMDP/HDP.m
+++ iPOMDP/HDP.m
@@ -1,5 +1,73 @@
-%% Dirichlet process (DP)
+%% Dirichlet process (DP) mixture model as Chinese restaurant process (CRP) mixture
+% directly yields cluster assignments of each observation
+% Following nomenclature of Teh 2010
 %
+
+rng default;
+
+H = @() rand(1,2) * 20; % base distribution = prior distribution over component parameters. Here, 2D uniform random variable in the square between [0, 0] and [10, 10] 
+F = @(theta) mvnrnd(theta, [1 0; 0 1]); % component distribution, parametrized by theta. Here, 2D Gaussain with fixed covariance and parametrized mean. So we have a 2D Gaussian mixture
+
+alpha = 10; % concentration parameter
+N = 50; % # observations
+K = 0; % # of active clusters
+
+n = []; % # of observations assigned to each cluster
+z = nan(N,1); % cluster assignment for each observation
+theta_star = []; % parameters for each cluster
+theta = nan(N,2); % parameters for each observation = theta_star{z(i)}
+x = nan(N,2);
+
+for i = 1:N % for each observation i
+    % with probability proportional to n(k), assign i to cluster k;
+    % with probability proportional to alpha, assign i to new cluster
+    %
+    p = [n(1:K), alpha];
+    p = p / sum(p);
+    
+    z(i) = find(mnrnd(1, p));
+    
+    if z(i) == K + 1
+        % new cluster
+        %
+        K = K + 1;
+        n(K) = 1;
+        theta_star(K,:) = H(); % draw new cluster params from base distribution
+    else
+        % old cluster
+        %
+        n(z(i)) = n(z(i)) + 1;
+    end
+    
+    assert(isequal(K, numel(n)));
+    assert(isequal(K, size(theta_star, 1)));
+    
+    % draw actual observation based on its cluster parameters
+    % this is where the "mixture" part comes in; up to now, it's just a DP
+    % with CRP construct
+    %
+    theta(i,:) = theta_star(z(i),:); % observation parameters = its cluster parameters
+    x(i,:) = F(theta(i,:)); % draw observation
+end
+
+
+% plot the clusters
+%
+C = colormap;
+C = C(randperm(size(C,1)),:); % use different color for each cluster
+
+figure;
+hold on;
+for k = 1:K % for each cluster
+    scatter(x(z == k, 1), x(z == k, 2), 4, C(k,:)); % plot all the observations
+    circle(theta_star(k,1), theta_star(k,2), 2, C(k,:)); % and a circle around the center (mean of observations)
+end
+hold off;
+
+%% DP mixture as stick-breaking construction (GEM)
+% yields probability of assigning each observation to a cluster
+%
+
 
 %% Hierarchical Dirichlet process (HDP)
 %
@@ -9,3 +77,10 @@
 
 %% hierarchical infinite HMM
 %
+
+function h = circle(x,y,r,c)
+    th = 0:pi/50:2*pi;
+    xunit = r * cos(th) + x;
+    yunit = r * sin(th) + y;
+    h = plot(xunit, yunit, 'Color', c);
+end
